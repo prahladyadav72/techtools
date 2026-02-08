bb0385e51f42237829d437ca5891285933017580
prahladyadav72 <prahladyadavcktd100@gmail.com>
2026-02-09 02:53:19 +0530

    Update tools-data.js

--- tools-data.js
+++ tools-data.js
@@ -1,52 +1,23 @@
 const tools = [
 
- {
- name:"QR Code Generator",
- desc:"Create QR codes instantly for links and text.",
+{
+name:"QR Code Generator",
+desc:"Create QR codes instantly for links and text.",
 
- link:"qr-generator.html"
- },
- {
- name:"Payslip Generator",
- desc:"Generate Your Payslip (Salary Slip) and Download",
-
- link:"payslip.html"
- },
-
-// {
-// name:"Password Generator",
-// desc:"Generate strong and secure passwords.",
-
-// link:"password-generator.html"
-// },
+link:"qr-generator.html"
+},
 
 
 
-// {
-// name:"Image Resizer",
-// desc:"Resize images online without quality loss.",
+{
+name:"Payslip Generator",
+desc:"Generate Your Payslip and Download",
 
-// link:"image-resizer.html"
-// },
+link:"payslip.html"
+}
 
-// {
-// name:"PDF to Word",
-// desc:"Convert PDF files to editable Word format.",
 
-// link:"pdf-to-word.html"
-// },
-
-// {
-// name:"JSON Formatter",
-// desc:"Format and validate JSON easily.",
-
-// link:"json-formatter.html"
-// }
 
  // Add all 40 tools here
 
 ];
-
-
-
-
